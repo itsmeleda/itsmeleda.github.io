65b41d43085bceae75f2241440e33106f056e562
Leda Grace <93599507+itsmeleda@users.noreply.github.com>
2023-06-28 15:09:35 +0800

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.slick-carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000 // Set the autoplay speed in milliseconds (e.g., 2000 = 2 seconds)
+  });
+});
